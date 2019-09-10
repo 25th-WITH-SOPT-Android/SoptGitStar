8928bd8feb46d0facef5d149803f573d3b7458ed
greedy0110 <greedy0110@gmail.com>
2019-09-10 17:17:48 +0900

    첫 커밋
    안드로이드 프로젝트 생성
    
    android studio 3.5
    androidx
    
    .gitignore 정리

---
+++ app/src/main/java/com/greedy0110/soptgitstar/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.greedy0110.soptgitstar
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
