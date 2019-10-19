e3dd43d69b04f8c7f84a4381a0d6b55879354b4b
greedy0110 <greedy0110@gmail.com>
2019-10-19 16:24:14 +0900

    Repository 를 비동기 처리를 하기 위해 Call 객체로 변환

--- app/src/main/java/com/greedy0110/soptgitstar/data/git_repo/DummyGitRepoRepository.kt
+++ app/src/main/java/com/greedy0110/soptgitstar/data/git_repo/DummyGitRepoRepository.kt
@@ -1,49 +1,53 @@
 package com.greedy0110.soptgitstar.data.git_repo
 
+import retrofit2.Call
+import retrofit2.mock.Calls
 import java.util.*
 
 class DummyGitRepoRepository : GitRepoRepository {
-    override fun getRepos(login: String): List<GitRepo> {
-        return listOf(
-            GitRepo(
-                name = "SoptStagram",
-                description = "IT 창업 동아리 SOPT 안드로이드 교육 프로젝트",
-                language = "Kotlin",
-                updated = Calendar.getInstance().apply {
-                    set(
-                        2019, Calendar.JANUARY, 10, 15, 12
-                    )
-                }.time
-            ),
-            GitRepo(
-                name = "artic_android",
-                description = "Forked from artic_development/artic_android",
-                language = "Kotlin",
-                updated = Calendar.getInstance().apply {
-                    set(
-                        2019, Calendar.AUGUST, 16, 15, 12
-                    )
-                }.time
-            ),
-            GitRepo(
-                name = "CalculatorWithEspresso",
-                description = "UI Test with Espresso + Unit Test Calculator",
-                language = "Kotlin",
-                updated = Calendar.getInstance().apply {
-                    set(
-                        2018, Calendar.OCTOBER, 10, 15, 12
-                    )
-                }.time
-            ),
-            GitRepo(
-                name = "problemSovingGuZongMan",
-                description = "프로그래밍 대회에서 배우는 알고리즘 문제해결전략 공부하자",
-                language = "Kotlin",
-                updated = Calendar.getInstance().apply {
-                    set(
-                        2018, Calendar.NOVEMBER, 10, 15, 12
-                    )
-                }.time
+    override fun getRepos(login: String): Call<List<GitRepo>> {
+        return Calls.response(
+            listOf(
+                GitRepo(
+                    name = "SoptStagram",
+                    description = "IT 창업 동아리 SOPT 안드로이드 교육 프로젝트",
+                    language = "Kotlin",
+                    updated = Calendar.getInstance().apply {
+                        set(
+                            2019, Calendar.JANUARY, 10, 15, 12
+                        )
+                    }.time
+                ),
+                GitRepo(
+                    name = "artic_android",
+                    description = "Forked from artic_development/artic_android",
+                    language = "Kotlin",
+                    updated = Calendar.getInstance().apply {
+                        set(
+                            2019, Calendar.AUGUST, 16, 15, 12
+                        )
+                    }.time
+                ),
+                GitRepo(
+                    name = "CalculatorWithEspresso",
+                    description = "UI Test with Espresso + Unit Test Calculator",
+                    language = "Kotlin",
+                    updated = Calendar.getInstance().apply {
+                        set(
+                            2018, Calendar.OCTOBER, 10, 15, 12
+                        )
+                    }.time
+                ),
+                GitRepo(
+                    name = "problemSovingGuZongMan",
+                    description = "프로그래밍 대회에서 배우는 알고리즘 문제해결전략 공부하자",
+                    language = "Kotlin",
+                    updated = Calendar.getInstance().apply {
+                        set(
+                            2018, Calendar.NOVEMBER, 10, 15, 12
+                        )
+                    }.time
+                )
             )
         )
     }
--- app/src/main/java/com/greedy0110/soptgitstar/feature/git_repo_list/GitRepoListActivity.kt
+++ app/src/main/java/com/greedy0110/soptgitstar/feature/git_repo_list/GitRepoListActivity.kt
@@ -3,16 +3,22 @@ package com.greedy0110.soptgitstar.feature.git_repo_list
 import android.graphics.Rect
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.util.TypedValue
 import android.view.View
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.greedy0110.soptgitstar.R
 import com.greedy0110.soptgitstar.data.git_repo.DummyGitRepoRepository
+import com.greedy0110.soptgitstar.data.git_repo.GitRepo
 import com.greedy0110.soptgitstar.data.git_repo.GitRepoRepository
 import com.greedy0110.soptgitstar.data.user.DummyUserRepository
+import com.greedy0110.soptgitstar.data.user.User
 import com.greedy0110.soptgitstar.data.user.UserRepository
 import kotlinx.android.synthetic.main.activity_git_repo_list.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class GitRepoListActivity : AppCompatActivity() {
     private lateinit var adapter: GitRepoAdapter
@@ -36,13 +42,26 @@ class GitRepoListActivity : AppCompatActivity() {
     }
 
     private fun makeProfile() {
-        // 현재 사용자 정보를 더미 데이터를 받아온다.
-        val currentUser = userRepository.getUser(login)
+        // 현재 사용자 정보를 비동기적으로 받아온다.
+        // Callback 내부의 코드는 나중에 데이터를 받아오고 실행된다.
+        userRepository.getUser(login).enqueue(object : Callback<User> {
+            override fun onFailure(call: Call<User>, t: Throwable) {
+                //네트워크 통신에 실패했을 때
+                Log.e("sopt_git_star", "error : $t")
+            }
 
-        // 더미 데이터를 이용해 Profile을 채운다.
-        profile_login.text = currentUser.login
-        profile_name.text = currentUser.name
-        profile_description.text = currentUser.bio
+            override fun onResponse(call: Call<User>, response: Response<User>) {
+                //네트워크 통신에 성공했을때, response 에 서버에서 받아온 데이터가 있을 것이다.
+                if (response.isSuccessful) {
+                    val currentUser = response.body()!! // TODO 여기서 body 가 없다면, 어플리케이션이 죽을 것이다 어떻게 해야할까?
+
+                    // 더미 데이터를 이용해 Profile을 채운다.
+                    profile_login.text = currentUser.login
+                    profile_name.text = currentUser.name
+                    profile_description.text = currentUser.bio
+                }
+            }
+        })
     }
 
     private fun makeGitRepoListView() {
@@ -63,8 +82,27 @@ class GitRepoListActivity : AppCompatActivity() {
             }
         })
 
-        adapter.data = gitRepoRepository.getRepos(login)
-        adapter.notifyDataSetChanged()
+        // 지정한 사용자의 팔로워 정보를 비동기적으로 받아온다.
+        // Callback 내부의 코드는 나중에 데이터를 받아오고 실행된다.
+        gitRepoRepository.getRepos(login).enqueue(object : Callback<List<GitRepo>> {
+            override fun onFailure(call: Call<List<GitRepo>>, t: Throwable) {
+                //네트워크 통신에 실패했을 때
+                Log.e("sopt_git_star", "error : $t")
+            }
+
+            override fun onResponse(call: Call<List<GitRepo>>, response: Response<List<GitRepo>>) {
+                //네트워크 통신에 성공했을때, response 에 서버에서 받아온 데이터가 있을 것이다.
+                if (response.isSuccessful) {
+                    val gitRepos = response.body()!! // TODO 여기서 body 가 없다면, 어플리케이션이 죽을 것이다 어떻게 해야할까?
+
+                    // adapter에 데이터 갱신하기
+                    // 데이터의 소스는 repository가 관리한다.
+                    adapter.data = gitRepos
+                    // 데이터 갱신한 다음, 어뎁터에게 다시 그리라고 알려준다. (이거 안하면 변화가 갱신되지 않는다.)
+                    adapter.notifyDataSetChanged()
+                }
+            }
+        })
     }
 
     private fun Int.dpToPx(): Int = TypedValue
