6fe177f3bbd16e1462a5d1e994e6201afb2a4472
greedy0110 <greedy0110@gmail.com>
2019-10-19 12:16:27 +0900

    list item git repo layout 재구성 하였다.

--- app/src/main/java/com/greedy0110/soptgitstar/feature/git_repo_list/GitRepoViewHolder.kt
+++ app/src/main/java/com/greedy0110/soptgitstar/feature/git_repo_list/GitRepoViewHolder.kt
@@ -8,12 +8,18 @@ import com.greedy0110.soptgitstar.R
 import com.greedy0110.soptgitstar.data.git_repo.GitRepo
 
 class GitRepoViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-    val imgListItemGitRepoOwner: ImageView = view.findViewById(R.id.imgListItemGitRepoOwner)
-    val txtListItemGitRepoName: TextView = view.findViewById(R.id.txtListItemGitRepoName)
-    val txtListItemGitRepoDesc: TextView = view.findViewById(R.id.txtListItemGitRepoDesc)
+
+    val title: TextView = view.findViewById(R.id.title)
+    val updated: TextView = view.findViewById(R.id.updated)
+    val description: TextView = view.findViewById(R.id.description)
+    val languageColor: ImageView = view.findViewById(R.id.language_color)
+    val language: TextView = view.findViewById(R.id.language)
 
     fun onBind(data: GitRepo) {
-        txtListItemGitRepoName.text = data.name
-        txtListItemGitRepoDesc.text = data.description
+        title.text = data.name
+        description.text = data.description
+        language.text = data.language
+        // TODO Upadted on 날짜 월 로 표현되도록 리팩토링 해야함
+        updated.text = data.updated.toString()
     }
 }
