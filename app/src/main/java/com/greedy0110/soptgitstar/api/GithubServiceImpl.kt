39d09d935d9b6733ef4089d0984f8d930d99762d
greedy0110 <greedy0110@gmail.com>
2019-10-19 17:46:32 +0900

    레트로핏 인터페이스를 빌더를 사용해 구현체 제작

---
+++ app/src/main/java/com/greedy0110/soptgitstar/api/GithubServiceImpl.kt
@@ -0,0 +1,15 @@
+package com.greedy0110.soptgitstar.api
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+// object 는 싱글톤 객체!
+object GithubServiceImpl {
+    private const val BASE_URL = "https://api.github.com"
+
+    val retrofit: Retrofit = Retrofit.Builder()
+        .baseUrl(BASE_URL)
+        // 인터넷 통신으로 받아온 정보를 Gson 을 통해서 시리얼라이즈 하겠다.
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+}
