9894ba88decee85026571e41d2fe8ae18f99e6d6
greedy0110 <greedy0110@gmail.com>
2019-10-23 16:17:01 +0900

    retrofit builder 를 사용해 retrofit 객체 만드는 것과 서비스 만드는 것 구분
    서비스가 다양화 될 수 있다.

--- app/src/main/java/com/greedy0110/soptgitstar/api/GithubServiceImpl.kt
+++ app/src/main/java/com/greedy0110/soptgitstar/api/GithubServiceImpl.kt
@@ -7,10 +7,11 @@ import retrofit2.converter.gson.GsonConverterFactory
 object GithubServiceImpl {
     private const val BASE_URL = "https://api.github.com"
 
-    val service: GithubService = Retrofit.Builder()
+    private val retrofit: Retrofit = Retrofit.Builder()
         .baseUrl(BASE_URL)
         // 인터넷 통신으로 받아온 정보를 Gson 을 통해서 시리얼라이즈 하겠다.
         .addConverterFactory(GsonConverterFactory.create())
         .build()
-        .create(GithubService::class.java)
+
+    val service: GithubService = retrofit.create(GithubService::class.java)
 }
